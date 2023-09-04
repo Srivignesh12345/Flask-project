2cc8fc9e911cb99d2b68b8b4741c7343cd8517f5
Srivignesh1234 <mailmetosrivignesh@gmail.com>
2023-09-04 09:07:22 +0530

    flask project

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask,jsonify,render_template, request, redirect
+from flask import Flask,jsonify,render_template,request, redirect
 from database import *
 
 app=Flask(__name__)
 
 app.config['MONGODB_HOST']=url
 mydata.init_app(app)
+
+@app.route("/erase/<mod>")
+def performDelete(mod):
+    collected=laptop.objects(model=mod).first()
+    collected.delete()
+    return redirect("/list")
+
+@app.route("/update/<mod>",methods=["GET","POST"])
+def performEdit(mod):
+    if request.method=="GET":
+        collected=laptop.objects(model=mod).first()
+        return render_template("edit.html",data=collected)
+    else:
+        model=request.form['model']
+        serial=request.form['serial']
+        ram=int(request.form['ram'])
+        ssd=int(request.form['ssd'])
+        price=int(request.form['price'])
+        stock=int(request.form['stock'])
+        type=request.form['type']
+
+        laptop.objects(model=model).update_one(set__serial=serial,set__ram=ram,
+                                               set__ssd=ssd,set__price=price,set__stock=stock,
+                                               set__type=type)
+        return redirect("/list")
+
+
 
 @app.route("/pick/<mod>")
 def showRead(mod):
@@ -14,18 +41,18 @@ def showRead(mod):
 @app.route("/new",methods=['GET','POST'])
 def newOne():
     if request.method=="GET":
-        return render_template("newlaptop.html")
+        return render_template("laptop.html")
     else:
-        laptop=laptop()
-        laptop.model=request.form['model']
-        laptop.serial=request.form['serial']
-        laptop.ram=int(request.form['ram'])
-        laptop.ssd=int(request.form['ssd'])
-        laptop.price=int(request.form['price'])
-        laptop.stock=int(request.form['stock'])
-        laptop.type=request.form['type']
+        lap=laptop()
+        lap.model=request.form['model']
+        lap.serial=request.form['serial']
+        lap.ram=int(request.form['ram'])
+        lap.ssd=int(request.form['ssd'])
+        lap.price=int(request.form['price'])
+        lap.stock=int(request.form['stock'])
+        lap.type=request.form['type']
         
-        laptop.save()
+        lap.save()
         
         return redirect("/list")
 
@@ -42,5 +69,31 @@ def listAll():
 def checkConnection():
     return jsonify(laptop.objects.all())
 
+@app.route("/shortlist",methods=['GET','POST'])
+def performFilter():
+    if request.method=="GET":
+        return render_template("filter.html")
+    else:
+        mod=request.form['model']
+        tp=request.form['type']
+        ram=request.form['ram']
+        cost=request.form['price']
+
+        if mod!="" and tp=="Select type" and ram=="" and cost=="":
+            collected=laptop.objects(model__startswith=mod)
+            return render_template("view.html",data=collected)
+        elif mod=="" and tp!="Select type" and ram=="" and cost=="":
+            collected=laptop.objects(type__iexact=tp)
+            return render_template("view.html",data=collected)
+        elif mod=="" and tp=="Select type" and ram!="" and cost=="":
+            ram=int(ram)
+            collected=laptop.objects(ram__gte=ram)
+            return render_template("view.html",data=collected)
+        elif mod=="" and tp=="Select type" and ram=="" and cost!="":
+            cost=int(cost)
+            collected=laptop.objects(price__lte=cost)
+            return render_template("view.html",data=collected)
+        else:
+            return render_template("filter.html")
 if __name__=="__main__":
     app.run(debug=True,port=2328)
