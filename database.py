aea55702d49de1acda3555c7b77ca82babea57eb
Srivignesh1234 <mailmetosrivignesh@gmail.com>
2023-09-04 10:22:36 +0530

    flask project

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
 from flask_mongoengine import MongoEngine
 
 mydata=MongoEngine()
-
 url="mongodb+srv://Srivignesh:sri12345*@cluster0.2pgxwru.mongodb.net/Srivignesh?retryWrites=true&w=majority"
-
 class laptop(mydata.Document):
    model=mydata.StringField()
    serial=mydata.StringField()
